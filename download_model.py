67ad8d68a791ba7fda2469c446b77cb5e30a789b
tim-hua-01 <70002232+tim-hua-01@users.noreply.github.com>
2025-09-14 04:55:00 +0000

    made sure the cache is in the right place

--- download_model.py
+++ download_model.py
@@ -1,5 +1,9 @@
 #%%
 import os
+os.environ["HF_HOME"] = "/workspace/.cache/huggingface"
+os.environ["HUGGINGFACE_HUB_CACHE"] = "/workspace/.cache/huggingface/hub"
+os.environ["HF_DATASETS_CACHE"] = "/workspace/.cache/huggingface/datasets"
+os.environ["TRANSFORMERS_CACHE"] = "/workspace/.cache/huggingface/hub"
 #os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 import time
 import torch as t
