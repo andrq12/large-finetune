d70297f99eaea0ee69db4fbdd521d01d93914d56
tim-hua-01 <70002232+tim-hua-01@users.noreply.github.com>
2025-09-22 03:05:19 +0000

    iteration round 4

--- download_model.py
+++ download_model.py
@@ -13,9 +13,7 @@ import numpy as np
 from tqdm.auto import tqdm
 from typing import Optional, List, Tuple, Literal, Any, cast
 from pathlib import Path
-from datasets import load_dataset
 from dotenv import load_dotenv
-import einops
 import datetime
 load_dotenv()
 from huggingface_hub import snapshot_download
@@ -24,18 +22,19 @@ REAL_PROMPTS = True
 
 #%%
 if __name__ == "__main__":
-    t.set_grad_enabled(False)
 
     model_id = "nvidia/Llama-3_3-Nemotron-Super-49B-v1" #nvidia/Llama-3_3-Nemotron-Super-49B-v1_5
     model_kwargs = {"torch_dtype": t.bfloat16, "trust_remote_code": True, "device_map": "cpu"}
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    tokenizer.pad_token_id = tokenizer.eos_token_id
-    tokenizer.padding_side = "left"
+    
+    
     #local_lora_path = snapshot_download(repo_id='andrewtim-mats/woodsolo_addon_coder_emoji_0.5epoch_sft_evalonly')
     local_lora_path2 = snapshot_download(repo_id='andrewtim-mats/woodsoloadd_codeonly_rt_add2')
-    #local_lora_path3 = snapshot_download(repo_id='andrewtim-mats/runs_coder_noed_doctok')
-
+    local_lora_path3 = snapshot_download(repo_id='timhua/woodsoloadd_codeonly_rt_add2_nsft_rd3')
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
     base_model = AutoModelForCausalLM.from_pretrained(model_id, **model_kwargs)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.padding_side = "left"
 
     print("\n"*5+'='*10 + "Models downloaded" + '='*10 + "\n"*5)
 
